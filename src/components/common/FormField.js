ed6b6ff8394aaee80fa82a65282cc614bdcab2bd
talkrz <krzysztof.talajko@gmail.com>
2019-01-06 16:09:20 +0100

    Fix grid mess in the form

--- src/components/common/FormField.js
+++ src/components/common/FormField.js
@@ -3,11 +3,15 @@ import React from 'react';
 export default function Field({ id, label, input, error }) {
   return (
     <div className="FormField" aria-label={id}>
-      <label htmlFor={id}>
-        {label}
-      </label>
-      {input(id)}
-      {error && <p className="ExpenseForm-error">{error}</p>}
+      <div className="FormField-field">
+        <label htmlFor={id}>
+          {label}
+        </label>
+        {input(id)}
+      </div>
+      <div className="FormField-error">
+        {error && <p className="FormField-error-message">{error}</p>}
+      </div>
     </div>
   )
 }
