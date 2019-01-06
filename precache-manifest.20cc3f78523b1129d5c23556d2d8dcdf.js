fcc1d8bf43ebe25248c621b51c4c192b233bd4dd
talkrz <krzysztof.talajko@gmail.com>
2019-01-06 15:52:09 +0100

    Updates

---
+++ precache-manifest.20cc3f78523b1129d5c23556d2d8dcdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d27c321de604519248",
+    "url": "/react-demo/static/js/runtime~main.d6d27c32.js"
+  },
+  {
+    "revision": "e8c0559c39dc4d2f9317",
+    "url": "/react-demo/static/js/main.e8c0559c.chunk.js"
+  },
+  {
+    "revision": "fadb4490196c25d7aa61",
+    "url": "/react-demo/static/js/1.fadb4490.chunk.js"
+  },
+  {
+    "revision": "e8c0559c39dc4d2f9317",
+    "url": "/react-demo/static/css/main.5d06f982.chunk.css"
+  },
+  {
+    "revision": "74d0b7479f30885bd93accd9f957b2af",
+    "url": "/react-demo/index.html"
+  }
+];
